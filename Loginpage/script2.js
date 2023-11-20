c36d4fe2e0b9a463801d14a5ed89f8662023befc
Patchanonss <104419006+Patchanonss@users.noreply.github.com>
2023-11-20 20:32:05 +0700

    Update script2.js

--- Loginpage/script2.js
+++ Loginpage/script2.js
@@ -3,12 +3,12 @@ correct_user = 'Real Username';
 correct_pass = '1q2w3e4r5t';
 
 function submitform(){
-    // alert('button is clickled')
     let f1 = document.getElementById('signupform');
     let user,pass;
-    user = document.getElementById('user1').value;
-    pass = document.getElementById('pass1').value;
+    user = document.getElementById('tempuser1').value;
+    pass = document.getElementById('temppass1').value;
     let temp1 = document.getElementById('incorrectcase');
+    var realtemp = document.querySelector('.tempo')
     if (user != correct_user){
         temp1.innerHTML = 'Invalid Username'
     }
@@ -17,8 +17,9 @@ function submitform(){
     }
     else {
         temp1.innerHTML = 'successfully log In'
+    realtemp.style.visibility = 'visible';
     }
-    // document.getElementById('showuser').innerHTML = user;
-    // document.getElementById('showpass').innerHTML = pass;
-
+}
+function closepopup() {
+    document.querySelector('.tempo').style.visibility = 'hidden';
 }
